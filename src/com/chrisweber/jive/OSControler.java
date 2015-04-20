1067c174c1c25a0e626eb40da1d68daed8ba38bc
Chris Weber <chris.l.weber@gmail.com>
2015-04-20 13:53:03 -0700

    Basic index page and action handeling

---
+++ src/com/chrisweber/jive/OSControler.java
@@ -0,0 +1,18 @@
+package com.chrisweber.jive;
+
+public class OSControler {
+
+private String name;
+	
+	public String execute() throws Exception {
+		return "success";
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
